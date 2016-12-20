e020c1559aa9a7751a3a57ae12b9d5785bb58222
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-12-19 16:56:47 -0800

    Add scss loaders and styling examples

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import components from './components';
 import services from './services';
+import './app.scss';
 
 const app = angular.module('angularApp', [components,services]);
 
